709928b1c543d15a342fad251fa98abb6d7b9372
Team Hackminors <146724977+teamhackminors@users.noreply.github.com>
2025-04-19 11:06:25 +0530

    Update mediaQueries.tsx
    removed the plugin

--- scripts/mediaQueries.tsx
+++ scripts/mediaQueries.tsx
@@ -1,19 +1,3 @@
-    var chatDiv = document.createElement('div');
-    chatDiv.id = 'botpressChat';
-    document.body.appendChild(chatDiv);
-
-    var script1 = document.createElement('script');
-    script1.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js';
-    script1.onload = function() {
-        // Once inject.js is loaded, load config.js
-        var script2 = document.createElement('script');
-        script2.src = 'https://mediafiles.botpress.cloud/c80ccde7-2db0-4ca9-99b0-14c91448ee4e/webchat/config.js';
-        script2.defer = true;
-        document.body.appendChild(script2);
-    };
-    document.body.appendChild(script1);
-    alert("Hi! This is to inform you that we have successfully plugged in our AI Chatbot, Chat2Eco, right at the bottom-right corner of your screen! You may check it out!");
-
 function MediaWidth(size: number) {
     var media = window.matchMedia("(max-width: " + size + "px)");
     return media.matches;
